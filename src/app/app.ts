888c6e6b5923dfb5980fa8ea8277a02e4b94147b
Erazmus1999 <erazmus1999@gmail.com>
2025-08-24 20:30:22 +0200

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,14 +3,16 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,             // 👈 TO DODAJ
   imports: [RouterOutlet],
   template: `
-    <h1>Welcome to {{ title() }}!</h1>
-
+    <h1>{{ sentence() }} {{ title() }}!</h1>
     <router-outlet />
   `,
-  styles: [],
+  styleUrls: ['./app.component.scss'],
 })
+
 export class App {
-  protected readonly title = signal('first-ng-app');
+  protected readonly title = signal('Jan');
+  protected readonly sentence = signal('Witaj serdecznie Panie: ')
 }
